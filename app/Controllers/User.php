5aa3f146c508099963abff4913ae79e730bb0da6
Neakles <tric201@gmail.com>
2023-06-29 18:42:05 +0700

    integrasi pembayaran midtrans

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -89,7 +89,7 @@ class User extends BaseController
             ->join('tagihan', 'tagihan.id_tagihan = pembayaran.id_tagihan');
     
         $data['user'] = $this->builder->get()->getRow();
-        
+        // dd($data['user']);
         // Menghitung jumlah bulan dan jumlah nominal jika status == 0
         $totalBulan = 0;
         $totalNominal = 0;
@@ -102,103 +102,5 @@ class User extends BaseController
         $data['nominal'] = $data['user']->nominal;
 
         return view('/user/pembayaran', $data);
-    }
-
-    public function token()
-    {
-        $this->builder
-            ->select('users.id, tagihan.bulan, nominal, pembayaran.status')
-            ->join('pembayaran', 'pembayaran.id_users = users.id')
-            ->join('tagihan', 'tagihan.id_tagihan = pembayaran.id_tagihan');
-
-        $data['user'] = $this->builder->get()->getRow();
-        // Menghitung jumlah bulan dan jumlah nominal jika status == 0
-        $totalBulan = 0;
-        $totalNominal = 0;
-        if (!empty($data['user']) && $data['user']->status == 0) {
-            $totalBulan = 1; // Set jumlah bulan ke 1 karena status == 0
-            $totalNominal = $data['user']->nominal * $totalBulan;
-        }
-        // $total_bulan = $totalBulan;
-        // $nominal = $data['user']->nominal;
-        // $total_nominal = $totalNominal;
-
-        $data['totalBulan'] = $totalBulan;
-        $data['totalNominal'] = $totalNominal;
-        $data['nominal'] = $data['user']->nominal;
-
-
-        $transaction_details = [
-            'order_id' => uniqid(),
-            'gross_amount' => $total_nominal, // no decimal allowed for creditcard
-        ];
-
-        // Optional
-        $item1_details = [
-            'id' => uniqid(),
-            'price' => $nominal,
-            'quantity' => $total_bulan,
-            'name' => 'bulan',
-        ];
-
-        // Optional
-
-        // Optional
-        $item_details = [$item1_details];
-
-        // Optional
-        $billing_address = [
-            'first_name' => $fullname,
-            'last_name' => 'a',
-            'address' => 'a',
-            'city' => 'a',
-            'postal_code' => 'a',
-            'phone' => 'a',
-            'country_code' => 'IDN',
-        ];
-
-        // Optional
-        $shipping_address = [
-            'first_name' => $fullname,
-            'address' => '',
-            'city' => '',
-            'postal_code' => '',
-            'phone' => $no_telp,
-            'country_code' => 'IDN',
-        ];
-
-        // Optional
-        $customer_details = [
-            'first_name' => $fullname,
-            'email' => $email,
-            'phone' => $no_telp,
-            'billing_address' => $billing_address,
-            'shipping_address' => $shipping_address,
-        ];
-
-        // Data yang akan dikirim untuk request redirect_url.
-        $credit_card['secure'] = true;
-        //ser save_card true to enable oneclick or 2click
-        //$credit_card['save_card'] = true;
-
-        $time = time();
-        $custom_expiry = [
-            'start_time' => date('Y-m-d H:i:s O', $time),
-            'unit' => 'minute',
-            'duration' => 60,
-        ];
-
-        $transaction_data = [
-            'transaction_details' => $transaction_details,
-            'item_details' => $item_details,
-            'customer_details' => $customer_details,
-            'credit_card' => $credit_card,
-            'expiry' => $custom_expiry,
-        ];
-
-        error_log(json_encode($transaction_data));
-        $snapToken = \Midtrans\Snap::getSnapToken($transaction_data);
-        error_log($snapToken);
-        echo $snapToken;
     }
 }
